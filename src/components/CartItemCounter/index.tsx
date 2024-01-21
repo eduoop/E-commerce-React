262abe260e1ffa2d4ab1b8e90ecdbcbb27866b4e
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-01-21 18:09:36 -0300

    fix: remove unused imports

--- src/components/CartItemCounter/index.tsx
+++ src/components/CartItemCounter/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 import {
